5c905e0d2a2ce3660ac85eaac15b119f316ba6a5
Thiago Pina <thiago_14sk8@hotmail.com>
2023-07-11 21:34:09 -0300

    Game seed

--- src/main/java/com/example/dslist/entities/Game.java
+++ src/main/java/com/example/dslist/entities/Game.java
@@ -22,23 +22,27 @@ public class Game {
 	
 	@Column(name = "game_year")
 	private Integer year;
-	private String platform;
+	private String platforms;
+	private double score;
 	private String imgUrl;
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, String genre, Integer year, String platform, String imgUrl,
+	public Game(Long id, String title, String genre, Integer year, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.genre = genre;
 		this.year = year;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -76,12 +80,20 @@ public class Game {
 		this.year = year;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+	
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
